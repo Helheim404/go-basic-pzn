c97abc18a38520a9d8b2a1958d5812d3c179d325
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-04-16 19:10:08 +0700

    added more elborate comment

--- string.go
+++ string.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("this is how you use string in golang using double quotes")
+	//function in string, which len("string") and "string"[number]. len for length and number for byte
+	//code below the length which is 56 include space
+	fmt.Println(len("this is how you use string in golang using double quotes"))
+	//code below going to print "t" as a byte and "h" as a byte
+	fmt.Println("this is how you use string in golang using double quotes"[0])
+	fmt.Println("this is how you use string in golang using double quotes"[1])
 }

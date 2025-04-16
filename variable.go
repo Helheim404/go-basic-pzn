58016267ab266fd4e456e446aad76a178a312bc8
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-04-16 19:42:22 +0700

    example ways to declare variable

--- variable.go
+++ variable.go
@@ -3,11 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var name string //string type variable
+	// two way of declaring variable
+	var name string
 
 	name = "Helheim 404"
 	fmt.Println(name)
 
 	name = "Goddess of Helheim"
 	fmt.Println(name) // this should be changed from "Helheim 404" to "Goddess of Helheim" because code is read from top to bottom
+
+	// another way of declaring variable
+	var username = "Helheim"
+	fmt.Println(username)
+
+	// var actually doesn't alwaysneed to be declared, but it's a good practice to declare it
+	// you can initialize the variable without var when you also declare it using " := " but only at the first time you declare it.
+	// like this code below
+	githubUsername := "Helheim"
+	fmt.Println(githubUsername)
+
+	githubUsername = "Goddess of Helheim"
+	fmt.Println(githubUsername)
+
+	// golang can make you declare multiple variables at once (nice) like this example below
+	var (
+		firstName = "Hello"
+		lastName  = "World"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
 }

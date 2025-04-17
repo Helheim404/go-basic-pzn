3ce8dfd4ebcc98eb43fb00be76c2823a18cc84da
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-04-17 19:07:10 +0700

    constant for dummies

---
+++ constant.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// constant is a variable that cannot be changed and cannot be redeclared, but it's not error when it's not used
+	const firstName string = "Helheim"
+	const lastName = "404"
+
+}

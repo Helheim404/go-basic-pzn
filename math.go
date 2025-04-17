4b8da9996d59e6fd4edcd6b52b415a0786f7f6e1
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-04-17 20:26:44 +0700

    augmented assignment 101 or operation with itself

--- math.go
+++ math.go
@@ -9,4 +9,24 @@ func main() {
 	var d = 5
 	var c = a + b/d
 	fmt.Println(c)
+	// + - * / and % for modulus
+
+	// modulus
+	var e = 10
+	var f = 3
+	var g = e % f
+	fmt.Println(g)
+
+	// augmented assignment or operation with itself (variable +=, -=, *=, /=, %=)
+	var h = 10
+	h += 10 // h = h + 10
+	fmt.Println(h)
+	h -= 15 // h = h - 5
+	fmt.Println(h)
+	h *= 5 // h = h * 5
+	fmt.Println(h)
+	h /= 5 // h = h / 5
+	fmt.Println(h)
+	h %= 5 // h = h % 5
+	fmt.Println(h)
 }

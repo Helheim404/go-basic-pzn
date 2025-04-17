1a1aa3f8f3057eaf5a1964ee0ca5836ea1fba8a1
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-04-17 19:09:27 +0700

    constant for another dummies

--- constant.go
+++ constant.go
@@ -1,8 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	// constant is a variable that cannot be changed and cannot be redeclared, but it's not error when it's not used
 	const firstName string = "Helheim"
 	const lastName = "404"
+
+	// const also can be declared for multiple variables at once
+	const (
+		username = "Goddess of Helheim"
+		password = "404"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(username)
+	fmt.Println(password)
+
+	// firstName = "Goddess of Helheim"
+	// lastName = "Hel of Helheim"
 
 }

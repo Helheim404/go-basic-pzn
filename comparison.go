fd224b8de2bc57dc36ad861438262c95e511debd
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-04-18 19:05:57 +0700

    basic comparison operator

---
+++ comparison.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// basic comparison operator (>, <, >=, <=, ==, !=)
+	var name1 = "Helheim"
+	var name2 = "Helheim"
+
+	var result bool = name1 != name2
+	fmt.Println(result)
+
+}

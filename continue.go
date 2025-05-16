7e424b29788f15b41e1d6f4055c28a1ca7640f37
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-05-17 00:20:15 +0700

    understanding the importance of continuation

---
+++ continue.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ecample of continue. continue simply like skipping some functions with set condition(s)
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("even loop ", i)
+	}
+}

59f054aff013035dd36bc556d8802011772e56bb
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-05-18 17:16:36 +0700

    tried: recover. no more Panic! At The Disco

--- panic.go
+++ panic.go
@@ -6,12 +6,16 @@ import (
 
 func endApp() {
 	fmt.Println("End Application")
+	// correct way to use recover, insert in defer function
+	message := recover()
+	fmt.Println("Panic is triggered, ", message)
 }
 
 func runApp(error bool) {
 	defer endApp()
 	if error {
 		// panic is for stopping the program completely
+		// to catch the error message, we need to use recover
 		panic("Whoops, something went wrong")
 	}
 }

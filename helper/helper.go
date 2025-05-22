db0c2ad9099b27efe43da5fbe0070f3e47d78dd4
Helheim404 <172242896+Helheim404@users.noreply.github.com>
2025-05-22 23:01:11 +0700

    tried: Access Modifier
    Uppercase for first alphabet to be able accessed globally and lowecase for locally inside the same package

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,14 @@
 package helper
 
+var (
+	version     = "1.0.0"
+	Application = "golang"
+)
+
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
 }
